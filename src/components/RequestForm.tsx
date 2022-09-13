54a6ccce4defb93b776dc2952dc4edee481aede2
rdnt42 <radiant42@yandex.ru>
2022-09-13 21:30:03 +0300

    #66 added request to server and proxy

--- src/components/RequestForm.tsx
+++ src/components/RequestForm.tsx
@@ -5,12 +5,24 @@ import {AutoCompleteSearch} from "./AutoCompleteSearch";
 import {InputCurrencySearch} from "./InputCurrencySearch";
 import {InputDaysSearch} from "./InputDaysSearch";
 import {DropDownComfort} from "./DropDownComfort";
+import axios from "axios";
 
 export const RequestForm = () => {
     const searchRequest = useContext(SearchContext);
 
     const sendRequest = () => {
-        console.log(searchRequest);
+        axios.get("/server/travel/api/v1/travel_map",
+            {
+                params: {
+                    departureCity: searchRequest.city,
+                    days: searchRequest.days,
+                    maxCost: searchRequest.budget,
+                    comfortType: searchRequest.comfortType
+                }
+            })
+            .then(response => {
+                console.log(response);
+            })
     }
 
     return (
@@ -19,7 +31,7 @@ export const RequestForm = () => {
             <InputCurrencySearch max={500000} step={5000}/>
             <InputDaysSearch max={30} step={1}/>
             <DropDownComfort/>
-            <Button label="Поиск" className="p-button-raised" onClick={sendRequest} />
+            <Button label="Поиск" className="p-button-raised" onClick={sendRequest}/>
         </div>
     )
 }
---
+++ src/setupProxy.js
@@ -0,0 +1,14 @@
+const {createProxyMiddleware} = require('http-proxy-middleware');
+
+module.exports = function(app) {
+    app.use(
+        '/server',
+        createProxyMiddleware({
+            target: process.env.REACT_APP_SERVER_HOST,
+            changeOrigin: true,
+            pathRewrite: {
+                '^/server': ''
+            }
+        }),
+    );
+};
